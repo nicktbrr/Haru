01704049108ec95d220c986a0b1513f67ac3ded1
Nick Barsi-Rhyne <nbarsirh@ucsc.edu>
2025-03-29 14:53:29 -0700

    updated connections

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,24 +1,25 @@
-import type React from "react"
-import "./globals.css"
-import { Quicksand } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "./globals.css";
+import { Quicksand } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const quicksand = Quicksand({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-quicksand",
-})
+});
 
 export const metadata = {
   title: "Haru - Cherry Blossom Video Generator",
-  description: "Transform your music into beautiful videos inspired by cherry blossoms",
-    generator: 'v0.dev'
-}
+  description:
+    "Transform your music into beautiful videos inspired by cherry blossoms",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -26,14 +27,17 @@ export default function RootLayout({
         className={`${quicksand.variable} font-sans bg-fixed bg-cover bg-center`}
         style={{ backgroundImage: 'url("/images/cherry-blossom-bg.png")' }}
       >
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
